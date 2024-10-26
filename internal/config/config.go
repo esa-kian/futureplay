98f6bd68d5fcf61aba8438a647688db3b34271d4
Esa <ehsan_amd72@hotmail.com>
2024-10-26 18:56:28 +0300

    create config file

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Port int `yaml:"port"`
+	} `yaml:"server"`
+	Matchmaking struct {
+		CompetitionSize int `yaml:"competition_size"`
+		WaitTimeSeconds int `yaml:"wait_time_seconds"`
+	} `yaml:"matchmaking"`
+}
+
+func LoadConfig(filePath string) (*Config, error) {
+	config := &Config{}
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(config)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Loaded configuration: %+v\n", config)
+	return config, nil
+}
